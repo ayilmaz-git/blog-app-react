57a65dff9360f38fdba347b08a017b4f30ab1251
aziz atilla yılmaz <atilla.yilmaz@ogr.iu.edu.tr>
2022-03-01 01:32:49 +0300

    Updates

---
+++ precache-manifest.378cce31d1d716fdd0b001d1a741517a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e2ee6a49ff6f707f797900ff92c9312",
+    "url": "/blog-app-react/index.html"
+  },
+  {
+    "revision": "0e64e32adb0635d52434",
+    "url": "/blog-app-react/static/css/main.6f685477.chunk.css"
+  },
+  {
+    "revision": "736e523d3c0eba286305",
+    "url": "/blog-app-react/static/js/2.22432119.chunk.js"
+  },
+  {
+    "revision": "4224b32d50aca7d0b4f248bf0384ed4f",
+    "url": "/blog-app-react/static/js/2.22432119.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e64e32adb0635d52434",
+    "url": "/blog-app-react/static/js/main.d5e24a01.chunk.js"
+  },
+  {
+    "revision": "0f8ffbfb93336da245fd",
+    "url": "/blog-app-react/static/js/runtime-main.44c67611.js"
+  }
+]);
